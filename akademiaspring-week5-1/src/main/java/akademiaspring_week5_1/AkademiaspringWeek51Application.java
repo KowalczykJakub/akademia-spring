1f1294416e28008708c9281c175d3c8112a43ea6
KowalczykJakub <79265889+KowalczykJakub@users.noreply.github.com>
2021-03-04 15:11:42 +0100

    Add files via upload

---
+++ akademiaspring-week5-1/src/main/java/akademiaspring_week5_1/AkademiaspringWeek51Application.java
@@ -0,0 +1,13 @@
+package akademiaspring_week5_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkademiaspringWeek51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkademiaspringWeek51Application.class, args);
+    }
+
+}
---
+++ akademiaspring-week5-1/src/main/java/akademiaspring_week5_1/CurrencyController.java
@@ -0,0 +1,31 @@
+package akademiaspring_week5_1;
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class CurrencyController {
+
+    private Currency getData() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        Currency currency = restTemplate.getForObject("https://api.exchangeratesapi.io/latest?base=USD", Currency.class);
+
+        return currency;
+
+    }
+
+    @GetMapping
+    public String getView(Model model) {
+
+        model.addAttribute("currency", getData());
+
+        return "view";
+
+    }
+
+}
